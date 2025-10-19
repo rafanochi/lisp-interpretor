a815005b3d75e2d862c5571e7ef098fc9cbee3c3
rafanochi <zawkindev@gmail.com>
2025-10-19 23:11:11 +0500

    solve 3rd exercise

--- main.hs
+++ main.hs
@@ -4,9 +4,6 @@ import Text.Printf
 
 main::IO()
 main = do
-  args <- getArgs
-  case args of
-    (x:y:_) -> printf "%s * %s = %s\n" x y $ show (read x * read y)
-    [x] -> putStrLn $ "Hello, " ++ x
-    [] -> putStrLn "no arguments at all"
-
+  putStrLn "Enter your name: "
+  name <- getLine 
+  putStrLn $ "your name is " ++ name

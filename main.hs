27459ef87e07c10c98789c01187f726831fae300
rafanochi <zawkindev@gmail.com>
2025-10-19 15:50:49 +0500

    perform operation to 2 arguments

--- main.hs
+++ main.hs
@@ -5,7 +5,7 @@ main::IO()
 main = do
   args <- getArgs
   case args of
-    (x:y:_) -> putStrLn $ "Hello, " ++ x ++ y
+    (x:y:_) -> putStrLn $ x ++  " * " ++ y ++ " = " ++ show (read x * read y) 
     [x] -> putStrLn $ "Hello, " ++ x
     [] -> putStrLn "no arguments at all"
 

8fe305eac0e46c1389d9bd1777bf34a52654e68a
rafanochi <zawkindev@gmail.com>
2025-10-19 15:56:22 +0500

    make it better

--- main.hs
+++ main.hs
@@ -1,11 +1,12 @@
 module Main where
 import System.Environment
+import Text.Printf
 
 main::IO()
 main = do
   args <- getArgs
   case args of
-    (x:y:_) -> putStrLn $ x ++  " * " ++ y ++ " = " ++ show (read x * read y) 
+    (x:y:_) -> printf "%s * %s = %s\n" x y $ show (read x * read y)
     [x] -> putStrLn $ "Hello, " ++ x
     [] -> putStrLn "no arguments at all"
 

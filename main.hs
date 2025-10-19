24e66702868a61da9c8f37b4404b7d27b682cf5c
rafanochi <zawkindev@gmail.com>
2025-10-19 15:42:52 +0500

    read upto 2 arguments

--- main.hs
+++ main.hs
@@ -4,5 +4,8 @@ import System.Environment
 main::IO()
 main = do
   args <- getArgs
-  putStrLn ("Hello, " ++ args !! 0)
+  case args of
+    (x:y:_) -> putStrLn $ "Hello, " ++ x ++ y
+    [x] -> putStrLn $ "Hello, " ++ x
+    [] -> putStrLn "no arguments at all"
 
